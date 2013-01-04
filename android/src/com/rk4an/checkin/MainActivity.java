8cd1a3ea267580a50770e352b70fb34bca2231bb
Erkan Valentin <valentin@clarinet.u-strasbg.fr>
2013-01-04 12:27:24 +0100

    support for text

--- android/src/com/rk4an/checkin/MainActivity.java
+++ android/src/com/rk4an/checkin/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.Menu;
 public class MainActivity extends DroidGap {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.loadUrl("file:///android_asset/www/index.html");
     }
